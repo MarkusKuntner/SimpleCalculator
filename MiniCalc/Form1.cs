1f11a3dd133e096b52f84fc5baddd795222d2684
Markus Kuntner <markus.kuntner@gmx.at>
2019-10-26 09:12:37 +0200

    nop

---
+++ MiniCalc/Form1.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Markus
+ * Date: 28.03.2016
+ * Time: 19:04
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Rechner
+{
+	/// <summary>
+	/// Description of Form1.
+	/// </summary>
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			label1.Text = "Mini Calculator by Markus Kuntner";
+			label2.Text = "Parser by artem@artemlos.net";
+			this.TopMost = true;
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+    }
+}
--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # SimpleCalculator<br>
 A simple calculator GUI using the Artem Los math expression parser with added dB (sound) functions<br>
 <p align="center">
-  <img src="Screenshot.png" width="250" title="hover text">
-  <img src="Screenshot2.png" width="250" title="hover text">
-  <img src="Screenshot3.png" width="250" title="hover text">
+  <img src="Screenshots\Screenshot1.png" width="250" title="hover text">
+  <img src="Screenshots\Screenshot2.png" width="250" title="hover text">
+  <img src="Screenshots\Screenshot3.png" width="250" title="hover text">
 </p>
 
+The GUI can be set to Always on Top. Move the calc by clicking to the area between the Float button and the "result" text box<br> 
 There are the following sound functions:<br>
 dbPlus(x1; x2)<br>
 dBMinus(x1; x2)<br>
