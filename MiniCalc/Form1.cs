70c3dd39b417d7c3f5350338551adab7a1c3067a
Markus Kuntner <markus.kuntner@gmx.at>
2019-12-13 18:57:07 +0100

    Further comments removed

--- MiniCalc/Form1.cs
+++ MiniCalc/Form1.cs
@@ -1,12 +1,4 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Markus
- * Date: 28.03.2016
- * Time: 19:04
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
 
